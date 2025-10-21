b473624d7cda588e418f0c1d67f0c614eddb0715
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-10-21 16:38:29 +0530

    completed art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,7 +15,8 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
-      <About/>
+      <About />
+      <Art/>
     </main>
   )
 }
